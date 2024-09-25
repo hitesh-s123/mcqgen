28b0c8bc28a3ccae20b0f040c9820731197b7370
Hitesh Shrivastav <hitesh.shrivastav@siemens.com>
2024-09-25 22:36:10 +0530

    made the hugging face pipeline

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'mcqgenerator',
+    version='0.0.1',
+    author='hitesh',
+    author_email='jnvpghitesh@gmail.com',
+    install_requires=['transformers','accelerate','tensorflow','tokenizers','langchain','streamlit','python-dotenv','PyPDF2','langchain_community'],
+    packages=find_packages()
+)
